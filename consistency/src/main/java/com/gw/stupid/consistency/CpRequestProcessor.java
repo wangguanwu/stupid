4ffba3f58f8b59c793f9b7de8354f68ecaffb0d7
wangguanwu <2531507093@qq.com>
2022-08-24 16:54:50 +0800

    [feat] jraft协议核心

--- consistency/src/main/java/com/gw/stupid/consistency/CpRequestProcessor.java
+++ consistency/src/main/java/com/gw/stupid/consistency/CpRequestProcessor.java
@@ -1,9 +1,5 @@
 package com.gw.stupid.consistency;
 
-import com.gw.stupid.consistency.entity.auto.CpResponse;
-import com.gw.stupid.consistency.entity.auto.ReadRequest;
-import com.gw.stupid.consistency.entity.auto.WriteRequest;
-
 /**
  * @author guanwu
  * @created on 2022-08-09 10:26:53
